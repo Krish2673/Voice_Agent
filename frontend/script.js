074d4611591713b376e5e849c61a699509fbc6b9
Krish2673 <krishpawar073@gmail.com>
2025-08-04 17:28:32 +0530

    Audio-Player

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,42 @@
-function showMsg() {
-    alert("Day 1 of building Voice Agent")
-}
+const Generate_btn = document.getElementById('generate_btn')
+
+Generate_btn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const textIP = document.getElementById('ip_text').value;
+
+    if(!textIP.trim()) {
+        alert("Please Enter some Text!")
+        return;
+    }
+
+    try {
+        const response = await fetch("/generate-audio", {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                text : textIP,
+                voice_id : "en-US-terrell"
+            })
+        });
+
+        if(!response.ok)
+            throw new Error("Failed to Generate Audio!")
+
+        const data = await response.json();
+        console.log("Fetched response:", data);
+        
+        const audioUrl = data.audio_url;
+        console.log("Audio URL:", audioUrl);
+        
+        const audioPlayer = document.getElementById("audioPlayer");
+        audioPlayer.src = audioUrl;
+        audioPlayer.style.display = "block";
+        audioPlayer.load();
+        audioPlayer.play();
+    }
+
+    catch(error) {
+        console.error("Error: ", error);
+        alert("Something went wrong. Please try again!")
+    }
+})
