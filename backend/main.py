41b465abab79f63c332474c5b6cdb25ebecf5be8
Krish2673 <krishpawar073@gmail.com>
2025-08-14 01:05:09 +0530

    Conversational Bot

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,9 @@ import assemblyai as aai
 from google import genai
 from typing import List,Dict
 
-# load_dotenv()
+load_dotenv()
 
-# aai.settings.api_key = os.getenv("AssemblyAI_API_KEY")
+aai.settings.api_key = os.getenv("AssemblyAI_API_KEY")
 
 uploadDIR = Path("uploads")
 uploadDIR.mkdir(exist_ok = True)
@@ -24,91 +24,6 @@ app.mount("/static", StaticFiles(directory="frontend"), name="static")
 async def get_index():
     with open("frontend/index.html", "r") as f:
         return f.read()
-
-class TTSRequest(BaseModel):
-    text: str
-    voice_id: str = "en-US-terrell"
-
-@app.post("/generate-audio")
-async def generate_audio(payload: TTSRequest):
-    client = Murf(api_key = os.getenv("MURF_API_KEY"))
-    res = client.text_to_speech.generate(
-        text=payload.text,
-        voice_id=payload.voice_id
-    )
-    return {"audio_url": res.audio_file}
-
-@app.post("/upload-audio")
-async def upload_audio(file : UploadFile = File(...)):
-    file_path = uploadDIR/file.filename
-
-    with open(file_path,"wb") as f:
-        content = await file.read()
-        f.write(content)
-
-    return {
-        "filename" : file.filename,
-        "content_type" : file.content_type,
-        "size_kb" : round(len(content)/1024,2)
-    }
-
-@app.post("/transcribe/file")
-async def transcribe_file(file : UploadFile = File(...)):
-    file_content = await file.read()
-
-    transcriber =  aai.Transcriber()
-    transcript = transcriber.transcribe(file_content)
-
-    return JSONResponse(content = {"transcript" : transcript.text})
-
-@app.post("/tts/echo")
-async def tts_echo(file : UploadFile = File(...)):
-    file_content = await file.read()
-
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(file_content)
-    text = transcript.text
-
-    client = Murf(api_key = os.getenv("MURF_API_KEY"))
-    res = client.text_to_speech.generate(
-        text = text,
-        voice_id = "en-US-terrell"
-    )
-    return {"audio_url" : res.audio_file, "transcript" : text}
-
-class LLMRequest(BaseModel):
-    prompt:str
-
-@app.post("/llm/query")
-async def llm_query(file : UploadFile = File(...)):
-    try:
-        file_content = await file.read()    #read audio file
-
-        transcriber = aai.Transcriber()         #Transcribe using Assesmbly AI
-        transcript = transcriber.transcribe(file_content)
-        user_text = transcript.text
-
-        client = genai.Client(api_key = os.getenv("Gemini_API_KEY"))        #Send Transcription to Gemini LLM
-        response = client.models.generate_content(
-            model = "gemini-2.5-flash",
-            contents = user_text 
-        )
-        llm_text = response.text
-
-        murf_client = Murf(api_key = os.getenv("MURF_API_KEY"))         # Send LLM Response to Murf tts
-        murf_res = murf_client.text_to_speech.generate(
-            text = llm_text,
-            voice_id = "en-US-terrell"
-        )
-
-        return JSONResponse(content = {                     # return text + audio
-            "user_transcript" : user_text,
-            "llm_text" : llm_text,
-            "audio_url" : murf_res.audio_file
-        })
-    
-    except Exception as e:
-        return JSONResponse(content = {"error" : str(e)}, status_code = 500)
     
 chat_histories : Dict[str,List[Dict[str,str]]] = {}
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,73 +1,39 @@
-const Generate_btn = document.getElementById('generate_btn');
+const chatBox = document.getElementById('chat-box');
 
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
-const resetBtn = document.getElementById('reset-btn');
-
-const playback = document.getElementById('echoPlayer');
-const recorder = document.getElementById('record-indicator');
-
-const statusDiv = document.getElementById('status');
-
-const transcriptContainer = document.getElementById('transHead');
-const Transcript = document.getElementById("transcription");
-
-const chatContainer = document.getElementById("chat-container")
-
-Generate_btn.addEventListener('click', async (e) => {
-    e.preventDefault();
-    const textIP = document.getElementById('ip_text').value;
-
-    if(!textIP.trim()) {
-        alert("Please Enter some Text!")
-        return;
-    }
-
-    try {
-        const response = await fetch("/generate-audio", {
-            method: "POST",
-            headers: {"Content-Type":"application/json"},
-            body: JSON.stringify({
-                text : textIP,
-                voice_id : "en-US-terrell"
-            })
-        });
-
-        if(!response.ok)
-            throw new Error("Failed to Generate Audio!")
-
-        const data = await response.json();
-        console.log("Fetched response:", data);
-        
-        const audioUrl = data.audio_url;
-        console.log("Audio URL:", audioUrl);
-        
-        const audioPlayer = document.getElementById("audioPlayer");
-        audioPlayer.src = audioUrl;
-        audioPlayer.style.display = "block";
-        audioPlayer.load();
-        audioPlayer.play();
-    }
-
-    catch(error) {
-        console.error("Error: ", error);
-        alert("Something went wrong. Please try again!")
-    }
-});
+const recordBtn = document.getElementById('record-btn');
+const playback = document.getElementById('audio-player');
 
 let mediaRecorder;
 let audioChunks = [];
+let isRecording = false;
+let isPlaying = false;
 
-startBtn.addEventListener('click', async() => {
+let session_id = new URLSearchParams(window.location.search).get("session_id");
+if(!session_id) {
+    session_id = Math.random().toString(36).substring(2,10);
+    window.history.replaceState({}, "", `?session_id=${session_id}`);
+}
+
+function appendMsg(sender,text) {
+    const msgDiv = document.createElement('div');
+    msgDiv.className = `message ${sender === "You" ? "user-msg" : "bot-msg"}`;
+    msgDiv.innerHTML = `<strong>${sender} : </strong> ${text}`
+    chatBox.appendChild(msgDiv);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+recordBtn.addEventListener('click', () => {
+    if(isPlaying) return;
+    if(!isRecording) startRecording();
+    else stopRecording();
+});
+
+async function startRecording() {
     try {
-        statusDiv.style.display = "none";
         const stream = await navigator.mediaDevices.getUserMedia({audio:true});
         
         mediaRecorder = new MediaRecorder(stream);
         audioChunks = [];
-
-        recorder.style.display = 'block';
-        startBtn.style.cursor = "not-allowed";
 
         mediaRecorder.ondataavailable = event => {
             if(event.data.size > 0) {
@@ -77,216 +43,92 @@ startBtn.addEventListener('click', async() => {
 
         mediaRecorder.onstop = () => {
             const audioBlob = new Blob(audioChunks, {type:"audio/webm"});
-            const audioUrl = URL.createObjectURL(audioBlob);
-
-            playback.src = audioUrl;
-
-            resetBtn.disabled = false;
-            resetBtn.style.cursor = "pointer";
-
-            recorder.style.display = 'none';
             
             askLLMWithMurf(audioBlob);
         };
 
         mediaRecorder.start();
-
-        startBtn.disabled = true;
-        stopBtn.disabled = false;
-        stopBtn.style.cursor = "pointer";
+        isRecording = true;
+        
+        recordBtn.textContent = "Stop";
+        recordBtn.classList.add('recording');        
     }
     catch (error){
-        console.error("Microphone access denied or error ocurred:", error);
-        alert("Microphone access is required to use Echo Bot!");
+        console.error("Microphone error ocurred:", error);
+        alert("Microphone access is required!");
     }
-});
+}
 
-stopBtn.addEventListener('click', () => {
-    if(mediaRecorder && mediaRecorder.state !== "inactive") {
+function stopRecording() {
+    if(mediaRecorder && isRecording) {
         mediaRecorder.stop();
-
-        startBtn.disabled = false;
-        startBtn.style.cursor = "pointer";
-
-        stopBtn.disabled = true;
-        stopBtn.style.cursor = "not-allowed";
+        isRecording = false;
+        recordBtn.textContent = "Start";
+        recordBtn.classList.remove('recording');
     }
-});
-
-resetBtn.addEventListener('click', () => {
-    audioChunks = [];
-    playback.src = "";
-
-    resetBtn.disabled = true;
-    resetBtn.style.cursor = "not-allowed";
-
-    stopBtn.disabled = true;
-    stopBtn.style.cursor = "not-allowed";
-
-    statusDiv.innerHTML = "";
-    statusDiv.style.display = "none";
-
-    transcriptContainer.style.display = "none";
-    Transcript.textContent = "";
-
-    chatContainer.style.display = "none";
-    chatContainer.innerHTML = "";
-});
-
-async function uploadAudio(blob) {
-    statusDiv.textContent = "Uploading...";
-
-    const formData = new FormData();
-    formData.append("file",blob,"recording.wav");
-
-    try {
-        const response = await fetch("/upload-audio", {
-            method : "POST",
-            body : formData
-        })
-
-        if(!response.ok) {
-            throw new Error(`Upload Failed: ${response.status}`);
-        }
-
-        const result = await response.json();
-
-        statusDiv.style.display = "block";
-        statusDiv.innerHTML = `
-        ✅ Upload successful <br>
-        Name : ${result.filename} <br>
-        Type : ${result.content_type} <br>
-        Size : ${result.size_kb} KB`;
-
-        await transcribeAudio(blob);
-    }
-    catch(error) {
-        statusDiv.textContent = `❌ Upload failed: " ${error.message}`;
-    }
-}
-
-async function transcribeAudio(file) {
-    transcriptContainer.style.display = "block";
-    Transcript.textContent = "Transcribing...";
-
-    const formData = new FormData();
-    formData.append("file",file);
-
-    try {
-        const res = await fetch("/transcribe/file", {
-            method : "POST",
-            body : formData,
-        });
-
-
-        if(!res.ok) {
-            throw new Error(`Failed to Transcribe ${res.status}`);
-        }
-
-        const data = await res.json();
-        const transcriptionText = data.transcript;
-
-        Transcript.textContent = transcriptionText || "No transcript available.";
-    }
-    catch(err) {
-        Transcript.textContent = `❌ Error: ${err.message}`;
-    }
-}
-
-async function echoWithMurf(blob) {
-    statusDiv.style.display = "block";
-    statusDiv.textContent = "Processing with Murf...";
-
-    const formData = new FormData();
-    formData.append("file",blob,"recording.wav");
-
-    try {
-        const response = await fetch("/tts/echo", {
-            method : "POST",
-            body : formData
-        });
-
-        if(!response.ok) {
-            throw new Error(`Echo failed : ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        transcriptContainer.style.display = "block";
-        Transcript.textContent = data.transcript || "No transcript available.";
-
-        playback.src = data.audio_url;
-        playback.load();
-        playback.play();
-
-        statusDiv.textContent = "✅ Murf echo complete!";
-
-    }   
-
-    catch(err) {
-        statusDiv.textContent = `❌ Error: ${err.message}`;
-    }
-}
-
-let session_id = new URLSearchParams(window.location.search).get("session_id");
-if(!session_id) {
-    session_id = Math.random().toString(36).substring(2,10);
-    window.history.replaceState({}, "", `?session_id = ${session_id}`);
 }
 
 let convoHistory = []
 
-function appendMsg(sender,text) {
-    const msgDiv = document.createElement('div');
-    msgDiv.classList.add(sender == "You" ? "user-msg" : "bot-msg");
-    msgDiv.innerHTML = `<strong>${sender} : </strong> ${text}`
-    chatContainer.appendChild(msgDiv);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-}
-
 async function askLLMWithMurf(blob) {
-    statusDiv.style.display = "block";
-    statusDiv.textContent = "Processing with Gemini + Murf";
-
-    chatContainer.style.display = "block";
-
-    const formData = new FormData();
-    formData.append("file",blob,"recording.wav");
+    recordBtn.disabled = true;
+    recordBtn.textContent = "Thinking...";
+    recordBtn.classList.add('thinking');
 
     try {
+        const formData = new FormData();
+        formData.append("file",blob,"recording.wav");
+
         const response = await fetch(`/agent/chat/${session_id}`, {
             method : "POST",
             body : formData
         });
         
-        const data = await response.json();
-
-        if(!response.ok) {
-            statusDiv.textContent = `❌ Error: ${data.error || "Unknown error"}`
-
-            if(data.audio_url) {
-                playback.src = data.audio_url;
-                playback.load();
-                playback.play();
-            }
+        if(response.status === 204) {
+            appendMsg("LLM","...No speech detected.")
             return;
         }
 
-        appendMsg("You",data.user_transcript);
-        appendMsg("LLM",data.llm_text);
+        const data = await response.json();
 
-        playback.src = data.audio_url;
-        playback.load();
-        playback.play();
+        if(!response.ok) {
+            appendMsg("LLM",data.error || "Something Went Wrong");
+            return;
+        }
+
+        if(data.audio_url) {
+                isPlaying = true;
+                playback.src = data.audio_url;
+                playback.load();
+                playback.play();
+                recordBtn.classList.remove('thinking');
+                recordBtn.textContent = "Start";
+        }
+        else {
+            recordBtn.disabled = false;
+            recordBtn.textContent = "Start";
+        }
+
+        appendMsg("You",data.user_transcript || "(No Transcript)");
+        appendMsg("LLM",data.llm_text || "(No response)");
 
         playback.onended = () => {
-            startBtn.click();
+            isPlaying = false;
+            recordBtn.disabled = false;
+            recordBtn.classList.remove('thinking');
+            recordBtn.textContent = "Start";
+            startRecording();
         }
-        
-        statusDiv.style.display = "none";
     }
 
     catch(err) {
-        statusDiv.textContent = `❌ Error: ${err.message}`;
+        console.error(err);
+        appendMsg("LLM",`❌ Error: ${err.message}`)
+        recordBtn.disabled = false;
+        recordBtn.textContent = "Start"
+    }
+
+    finally {
+        recordBtn.classList.remove('thinking');
     }
 }
