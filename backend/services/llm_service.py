73727aa4c031e521b9a311ad7cd58f6a88b788d0
Krish2673 <krishpawar073@gmail.com>
2025-08-29 23:55:38 +0530

    cleanin

--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -1,11 +1,11 @@
-from google import genai
-from backend.utils.config import GEMINI_API_KEY
+# from google import genai
+# from backend.utils.config import GEMINI_API_KEY
 
-client = genai.Client(api_key=GEMINI_API_KEY)
+# client = genai.Client(api_key=GEMINI_API_KEY)
 
-def generate_llm_response(conversation: str) -> str:
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=conversation
-    )
-    return response.text
+# def generate_llm_response(conversation: str) -> str:
+#     response = client.models.generate_content(
+#         model="gemini-2.5-flash",
+#         contents=conversation
+#     )
+#     return response.text
--- backend/services/stt_service.py
+++ backend/services/stt_service.py
@@ -1,9 +1,9 @@
-import assemblyai as aai
-from backend.utils.config import ASSEMBLYAI_API_KEY
+# import assemblyai as aai
+# from backend.utils.config import ASSEMBLYAI_API_KEY
 
-aai.settings.api_key = ASSEMBLYAI_API_KEY
+# aai.settings.api_key = ASSEMBLYAI_API_KEY
 
-def transcribe_audio(audio_bytes: bytes) -> str:
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(audio_bytes)
-    return transcript.text
+# def transcribe_audio(audio_bytes: bytes) -> str:
+#     transcriber = aai.Transcriber()
+#     transcript = transcriber.transcribe(audio_bytes)
+#     return transcript.text
